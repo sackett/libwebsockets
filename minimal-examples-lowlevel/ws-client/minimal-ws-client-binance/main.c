db4066f8bb3543e580b430679f641aa26f1c99cf
m <umerta@gmail.com>
2022-12-23 18:04:20 +0300

    “kucoin”

--- minimal-examples-lowlevel/ws-client/minimal-ws-client-binance/main.c
+++ minimal-examples-lowlevel/ws-client/minimal-ws-client-binance/main.c
@@ -15,12 +15,13 @@
 #include <signal.h>
 #include <ctype.h>
 
-typedef struct range {
-	uint64_t		sum;
-	uint64_t		lowest;
-	uint64_t		highest;
+typedef struct range
+{
+	uint64_t sum;
+	uint64_t lowest;
+	uint64_t highest;
 
-	unsigned int		samples;
+	unsigned int samples;
 } range_t;
 
 /*
@@ -28,15 +29,16 @@ typedef struct range {
  * the client connection bound to it
  */
 
-static struct my_conn {
-	lws_sorted_usec_list_t	sul;	     /* schedule connection retry */
-	lws_sorted_usec_list_t	sul_hz;	     /* 1hz summary */
+static struct my_conn
+{
+	lws_sorted_usec_list_t sul;	   /* schedule connection retry */
+	lws_sorted_usec_list_t sul_hz; /* 1hz summary */
 
-	range_t			e_lat_range;
-	range_t			price_range;
+	range_t e_lat_range;
+	range_t price_range;
 
-	struct lws		*wsi;	     /* related wsi if any */
-	uint16_t		retry_count; /* count of consequetive retries */
+	struct lws *wsi;	  /* related wsi if any */
+	uint16_t retry_count; /* count of consequetive retries */
 } mco;
 
 static struct lws_context *context;
@@ -47,7 +49,7 @@ static int interrupted;
  * OpenSSL uses the system trust store.  mbedTLS / WolfSSL have to be told which
  * CA to trust explicitly.
  */
-static const char * const ca_pem_digicert_global_root =
+static const char *const ca_pem_digicert_global_root =
 	"-----BEGIN CERTIFICATE-----\n"
 	"MIIDrzCCApegAwIBAgIQCDvgVpBCRrGhdWrJWZHHSjANBgkqhkiG9w0BAQUFADBh\n"
 	"MQswCQYDVQQGEwJVUzEVMBMGA1UEChMMRGlnaUNlcnQgSW5jMRkwFwYDVQQLExB3\n"
@@ -76,17 +78,17 @@ static const char * const ca_pem_digicert_global_root =
  * The retry and backoff policy we want to use for our client connections
  */
 
-static const uint32_t backoff_ms[] = { 1000, 2000, 3000, 4000, 5000 };
+static const uint32_t backoff_ms[] = {1000, 2000, 3000, 4000, 5000};
 
 static const lws_retry_bo_t retry = {
-	.retry_ms_table			= backoff_ms,
-	.retry_ms_table_count		= LWS_ARRAY_SIZE(backoff_ms),
-	.conceal_count			= LWS_ARRAY_SIZE(backoff_ms),
+	.retry_ms_table = backoff_ms,
+	.retry_ms_table_count = LWS_ARRAY_SIZE(backoff_ms),
+	.conceal_count = LWS_ARRAY_SIZE(backoff_ms),
 
-	.secs_since_valid_ping		= 400,  /* force PINGs after secs idle */
-	.secs_since_valid_hangup	= 400, /* hangup after secs idle */
+	.secs_since_valid_ping = 400,	/* force PINGs after secs idle */
+	.secs_since_valid_hangup = 400, /* hangup after secs idle */
 
-	.jitter_percent			= 0,
+	.jitter_percent = 0,
 };
 
 /*
@@ -104,18 +106,18 @@ static const lws_retry_bo_t retry = {
  */
 
 static const struct lws_extension extensions[] = {
-	{
-		"permessage-deflate",
-		lws_extension_callback_pm_deflate,
-		"permessage-deflate"
-		 "; client_no_context_takeover"
-		 "; client_max_window_bits"
-	},
-	{ NULL, NULL, NULL /* terminator */ }
-};
+	{"permessage-deflate",
+	 lws_extension_callback_pm_deflate,
+	 "permessage-deflate"
+	 "; client_no_context_takeover"
+	 "; client_max_window_bits"},
+	{NULL, NULL, NULL /* terminator */}};
 /*
  * Scheduled sul callback that starts the connection attempt
  */
+
+static char path[512] = {0};
+static char address[512] = {0};
 
 static void
 connect_client(lws_sorted_usec_list_t *sul)
@@ -127,9 +129,9 @@ connect_client(lws_sorted_usec_list_t *sul)
 
 	i.context = context;
 	i.port = 443;
-	i.address = "fstream.binance.com";
-	i.path = "/stream?"
-		 "streams=btcusdt@depth@0ms/btcusdt@bookTicker/btcusdt@aggTrade";
+	// i.address = "fstream.binance.com";
+	i.address = address;
+	i.path = path;
 	i.host = i.address;
 	i.origin = i.address;
 	i.ssl_connection = LCCSCF_USE_SSL | LCCSCF_PRIORITIZE_READS;
@@ -146,7 +148,8 @@ connect_client(lws_sorted_usec_list_t *sul)
 		 * point.
 		 */
 		if (lws_retry_sul_schedule(context, 0, sul, &retry,
-					   connect_client, &mco->retry_count)) {
+								   connect_client, &mco->retry_count))
+		{
 			lwsl_err("%s: connection attempts exhausted\n", __func__);
 			interrupted = 1;
 		}
@@ -180,24 +183,26 @@ sul_hz_cb(lws_sorted_usec_list_t *sul)
 	 */
 
 	lws_sul_schedule(lws_get_context(mco->wsi), 0, &mco->sul_hz,
-			 sul_hz_cb, LWS_US_PER_SEC);
+					 sul_hz_cb, LWS_US_PER_SEC);
 
 	if (mco->price_range.samples)
 		lwsl_notice("%s: price: min: %llu¢, max: %llu¢, avg: %llu¢, "
-			    "(%d prices/s)\n",
-			    __func__,
-			    (unsigned long long)mco->price_range.lowest,
-			    (unsigned long long)mco->price_range.highest,
-			    (unsigned long long)(mco->price_range.sum / mco->price_range.samples),
-			    mco->price_range.samples);
+					"(%d prices/s)\n",
+					__func__,
+					(unsigned long long)mco->price_range.lowest,
+					(unsigned long long)mco->price_range.highest,
+					(unsigned long long)(mco->price_range.sum / mco->price_range.samples),
+					mco->price_range.samples);
 	if (mco->e_lat_range.samples)
 		lwsl_notice("%s: elatency: min: %llums, max: %llums, "
-			    "avg: %llums, (%d msg/s)\n", __func__,
-			    (unsigned long long)mco->e_lat_range.lowest / 1000,
-			    (unsigned long long)mco->e_lat_range.highest / 1000,
-			    (unsigned long long)(mco->e_lat_range.sum /
-					   mco->e_lat_range.samples) / 1000,
-			    mco->e_lat_range.samples);
+					"avg: %llums, (%d msg/s)\n",
+					__func__,
+					(unsigned long long)mco->e_lat_range.lowest / 1000,
+					(unsigned long long)mco->e_lat_range.highest / 1000,
+					(unsigned long long)(mco->e_lat_range.sum /
+										 mco->e_lat_range.samples) /
+						1000,
+					mco->e_lat_range.samples);
 
 	range_reset(&mco->e_lat_range);
 	range_reset(&mco->price_range);
@@ -218,7 +223,7 @@ pennies(const char *s)
 
 static int
 callback_minimal(struct lws *wsi, enum lws_callback_reasons reason,
-		 void *user, void *in, size_t len)
+				 void *user, void *in, size_t len)
 {
 	struct my_conn *mco = (struct my_conn *)user;
 	uint64_t latency_us, now_us;
@@ -227,11 +232,12 @@ callback_minimal(struct lws *wsi, enum lws_callback_reasons reason,
 	const char *p;
 	size_t alen;
 
-	switch (reason) {
+	switch (reason)
+	{
 
 	case LWS_CALLBACK_CLIENT_CONNECTION_ERROR:
 		lwsl_err("CLIENT_CONNECTION_ERROR: %s\n",
-			 in ? (char *)in : "(null)");
+				 in ? (char *)in : "(null)");
 		goto do_retry;
 		break;
 
@@ -240,12 +246,13 @@ callback_minimal(struct lws *wsi, enum lws_callback_reasons reason,
 		 * The messages are a few 100 bytes of JSON each
 		 */
 
-		// lwsl_hexdump_notice(in, len);
+		lwsl_hexdump_notice(in, len);
+		// lwsl_notice("%s", (const char *)in);
 
 		now_us = (uint64_t)get_us_timeofday();
 
 		p = lws_json_simple_find((const char *)in, len,
-					 "\"depthUpdate\"", &alen);
+								 "\"depthUpdate\"", &alen);
 		/*
 		 * Only the JSON with depthUpdate init has the numbers we care
 		 * about as well
@@ -254,13 +261,14 @@ callback_minimal(struct lws *wsi, enum lws_callback_reasons reason,
 			break;
 
 		p = lws_json_simple_find((const char *)in, len, "\"E\":", &alen);
-		if (!p) {
+		if (!p)
+		{
 			lwsl_err("%s: no E JSON\n", __func__);
 			break;
 		}
 		lws_strnncpy(numbuf, p, alen, sizeof(numbuf));
 		latency_us = now_us -
-				((uint64_t)atoll(numbuf) * LWS_US_PER_MS);
+					 ((uint64_t)atoll(numbuf) * LWS_US_PER_MS);
 
 		if (latency_us < mco->e_lat_range.lowest)
 			mco->e_lat_range.lowest = latency_us;
@@ -271,8 +279,9 @@ callback_minimal(struct lws *wsi, enum lws_callback_reasons reason,
 		mco->e_lat_range.samples++;
 
 		p = lws_json_simple_find((const char *)in, len,
-					 "\"a\":[[\"", &alen);
-		if (p) {
+								 "\"a\":[[\"", &alen);
+		if (p)
+		{
 			lws_strnncpy(numbuf, p, alen, sizeof(numbuf));
 			price = pennies(numbuf);
 
@@ -289,7 +298,7 @@ callback_minimal(struct lws *wsi, enum lws_callback_reasons reason,
 	case LWS_CALLBACK_CLIENT_ESTABLISHED:
 		lwsl_user("%s: established\n", __func__);
 		lws_sul_schedule(lws_get_context(wsi), 0, &mco->sul_hz,
-				 sul_hz_cb, LWS_US_PER_SEC);
+						 sul_hz_cb, LWS_US_PER_SEC);
 		mco->wsi = wsi;
 		range_reset(&mco->e_lat_range);
 		range_reset(&mco->price_range);
@@ -317,7 +326,8 @@ do_retry:
 	 * delay plus the random jitter amount.
 	 */
 	if (lws_retry_sul_schedule_retry_wsi(wsi, &mco->sul, connect_client,
-					     &mco->retry_count)) {
+										 &mco->retry_count))
+	{
 		lwsl_err("%s: connection attempts exhausted\n", __func__);
 		interrupted = 1;
 	}
@@ -326,9 +336,8 @@ do_retry:
 }
 
 static const struct lws_protocols protocols[] = {
-	{ "lws-minimal-client", callback_minimal, 0, 0, 0, NULL, 0 },
-	LWS_PROTOCOL_LIST_TERM
-};
+	{"lws-minimal-client", callback_minimal, 0, 0, 0, NULL, 0},
+	LWS_PROTOCOL_LIST_TERM};
 
 static void
 sigint_handler(int sig)
@@ -340,12 +349,16 @@ int main(int argc, const char **argv)
 {
 	struct lws_context_creation_info info;
 	int n = 0;
+	FILE *f;
+	const char *p;
+	char buf[1024], token[392] = {0};
+	size_t len, alen;
 
 	signal(SIGINT, sigint_handler);
 	memset(&info, 0, sizeof info);
 	lws_cmdline_option_handle_builtin(argc, argv, &info);
 
-	lwsl_user("LWS minimal binance client\n");
+	lwsl_user("LWS minimal kucoin client\n");
 
 	info.options = LWS_SERVER_OPTION_DO_SSL_GLOBAL_INIT;
 	info.port = CONTEXT_PORT_NO_LISTEN; /* we do not run any server */
@@ -362,8 +375,42 @@ int main(int argc, const char **argv)
 	info.client_ssl_ca_mem_len = (unsigned int)strlen(ca_pem_digicert_global_root);
 #endif
 
+	f = fopen("bullet-public.json", "r");
+	if (f && fgets(buf, sizeof(buf), f))
+	{
+		len = strlen(buf);
+
+		p = lws_json_simple_find((const char *)buf, len, "\"token\":", &alen);
+		if (p)
+		{
+			strncpy(token, p, alen);
+			snprintf(path, sizeof(path), "?token=%s", token);
+			lwsl_user("path=%s\n", path);
+		}
+		else
+		{
+			lwsl_err("no token!\n");
+			return 1;
+		}
+
+		p = lws_json_simple_find((const char *)buf, len, "\"endpoint\":\"wss://", &alen);
+		if (p)
+		{
+			strncpy(address, p, alen - 2);
+			lwsl_user("address=%s\n", address);
+		}
+		else
+		{
+			lwsl_err("no address!\n");
+			return 1;
+		}
+
+		fclose(f);
+	}
+
 	context = lws_create_context(&info);
-	if (!context) {
+	if (!context)
+	{
 		lwsl_err("lws init failed\n");
 		return 1;
 	}
@@ -379,4 +426,3 @@ int main(int argc, const char **argv)
 
 	return 0;
 }
-
---
+++ minimal-examples-lowlevel/ws-client/minimal-ws-client-binance/run.sh
@@ -0,0 +1,2 @@
+curl -d "" https://api.kucoin.com/api/v1/bullet-public > bullet-public.json
+./lws-minimal-ws-client-binance
